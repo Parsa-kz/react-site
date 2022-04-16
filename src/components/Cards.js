d21da73ab13a72d968440ee54c7c51bb9bb6a410
Parsa Karimzadeh <skarimzadeh99@gmail.com>
2022-04-16 13:34:25 +0430

    done

---
+++ src/components/Cards.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styles from "./Cards.module.css";
+import Card from "./Card";
+import iphonex from "../images/iphonex.png"
+
+class Cards extends Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <Card image={iphonex} name="iphone X" cost="500$" />
+                <Card image={iphonex} name="iphone X" cost="500$" />
+                <Card image={iphonex} name="iphone X" cost="500$" />
+                <Card image={iphonex} name="iphone X" cost="500$" />
+            </div>
+        );
+    }
+}
+
+export default Cards;
