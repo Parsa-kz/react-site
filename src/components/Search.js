8d4152a317a0f3856e173eaf20ac805169e1512d
Parsa Karimzadeh <skarimzadeh99@gmail.com>
2022-04-19 23:34:23 +0430

    using events

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import styles from "./Search.module.css";
 
 class Search extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            text: ""
+        }
+    }
+
+    changeHandler = (event) => {
+        this.setState({
+            text: event.target.value,
+        })
+    }
+
     render() {
         return (
             <div className={styles.container}>
                 <p>Search what you want</p>
-                <input placeholder="Search..." />
+                <div>
+                    <input type="text" value={this.state.text} onChange={this.changeHandler} placeholder="Search..." />
+                    <span>{this.state.text}</span>
+                </div>
             </div>
         );
     }
